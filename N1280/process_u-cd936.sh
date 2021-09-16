69fd9da0c4fb35c2bbfc88f370cb016591701b65
Annette Osprey <4313200+aosprey@users.noreply.github.com>
2021-09-16 16:19:39 +0100

    Update A2 script.

--- N1280/process_u-cd936.sh
+++ N1280/process_u-cd936.sh
@@ -9,16 +9,10 @@ SUITEID=u-cd936
 logdir=${DATA1}/${SUITEID}
 
 cylc_data_raw=${DATA2}/raw/${SUITEID}_cylc.csv
-#safe_data_raw=${DATA2}/raw/annette_sep20_safe.csv
 
 cylc_data_processed=${DATA2}/processed/${SUITEID}_cylc_all.csv
-#safe_data_processed=${DATA2}/processed/${SUITEID}_safe.csv
 
 task_name=atmos_main
 
-#${SCRIPTDIR}/parse_job_status_all.sh $logdir $task_name $cylc_data_raw
-
 python ${SCRIPTDIR}/process_cylc_times.py --keep_repeats $cylc_data_raw $cylc_data_processed 
-
-#python ${SCRIPTDIR}/process_safe_logs.py $safe_data_raw $cylc_data_processed $safe_data_processed
 
