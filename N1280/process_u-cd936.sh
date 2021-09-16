b3c5f7492bd1afaab2ed06edfed3e1564798327d
Annette Osprey <4313200+aosprey@users.noreply.github.com>
2021-09-16 16:48:31 +0100

    Tidy up.

--- N1280/process_u-cd936.sh
+++ N1280/process_u-cd936.sh
@@ -5,14 +5,12 @@ SCRIPTDIR=/home/annette/Work/HRCM/run_analysis/scripts
 DATA1=/home/annette/Data/HRCM
 DATA2=/home/annette/Work/HRCM/run_analysis/N1280/files
 SUITEID=u-cd936
-
-logdir=${DATA1}/${SUITEID}
-
-cylc_data_raw=${DATA2}/raw/${SUITEID}_cylc.csv
-
-cylc_data_processed=${DATA2}/processed/${SUITEID}_cylc_all.csv
-
 task_name=atmos_main
 
+logdir=${DATA1}/${SUITEID}
+cylc_data_raw=${DATA2}/raw/${SUITEID}_cylc.csv
+cylc_data_processed=${DATA2}/processed/${SUITEID}_cylc_all.csv
+
+${SCRIPTDIR}/parse_job_status.sh $logdir $task_name $cylc_data_raw
 python ${SCRIPTDIR}/process_cylc_times.py --keep_repeats $cylc_data_raw $cylc_data_processed 
 
