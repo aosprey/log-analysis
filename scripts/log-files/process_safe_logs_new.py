b7a4a99933f4ee0279cd56ab825b5cf9261be069
Annette Osprey <4313200+aosprey@users.noreply.github.com>
2021-09-22 16:18:47 +0100

    Script for processing new SAFE reports.

---
+++ scripts/log-files/process_safe_logs_new.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Process data from Archer SAFE reports.
+# Should be in CSV format.
+# Cross reference against PBS job ids for specific suite.
+
+# Arguments: safe_file, cylc_file, out_file
+
+import pandas as pd
+import sys
+
+if len(sys.argv) < 4:
+    sys.exit("Need 3 arguments: SAFE file, cylc file and output file")
+safe_file = sys.argv[1]
+cylc_file = sys.argv[2]
+out_file = sys.argv[3]
+
+# Read in cylc logs
+cylc = pd.read_csv(cylc_file, index_col='PBS ID', skipinitialspace=True)
+
+# Read in SAFE data
+safe = pd.read_csv(safe_file, skiprows=4, index_col='Batch ID')
+safe.rename_axis('PBS ID', inplace=True)
+
+# Drop jobs that don't appear in cylc logs
+reference_ids = cylc.index.values
+valid = safe.index.isin(reference_ids)
+safe = safe[valid]
+
+# May need to do some extra processing here ??
+
+# Write out processed file 
+safe.to_csv(out_file)
